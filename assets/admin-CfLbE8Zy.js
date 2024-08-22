f196a8e4b27b064f0726f7c06370ad675dfca96c
shuyu626 <164139722+shuyu626@users.noreply.github.com>
2024-08-22 05:37:07 +0000

    Deploying to gh-pages from @ shuyu626/final_front@1da48dd8f2401b16e420bf14b5de7fc4c5a0a104 🚀

--- assets/admin-CfLbE8Zy.js
+++
@@ -1,1 +0,0 @@
-import{V as o}from"./VMain-BK_Rdvo-.js";import{r as a,o as r,c as n,a as s,b as e,w as c,F as m}from"./index-B_MsKRGK.js";import"./ssrBoot-BdhYeogb.js";const f={__name:"admin",setup(_){return(l,p)=>{const t=a("router-view");return r(),n(m,null,[s("d "),e(o,null,{default:c(()=>[e(t)]),_:1})],64)}}};export{f as default};
