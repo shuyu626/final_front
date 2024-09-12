95e1bb4e77ab0771b342b38cbf2d22c711060490
shuyu626 <164139722+shuyu626@users.noreply.github.com>
2024-09-12 14:00:45 +0000

    Deploying to gh-pages from @ shuyu626/final_front@29a21f59c7c5006cb37cd7f47c48dbea2b25d37c 🚀

--- assets/submitButton-BAUZr_V_.js
+++
@@ -1,1 +0,0 @@
-import{o as e,e as n,m as o}from"./index-CSljMwmO.js";const c={__name:"submitButton",props:{text:{type:String,default:"新增"}},setup(t){return(a,r)=>(e(),n(o,{text:t.text,variant:"text",class:"rounded-xl b-1",density:"comfortable",id:"button"},null,8,["text"]))}};export{c as _};
