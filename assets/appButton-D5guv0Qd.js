52d62d7a304531874ad3358157d6242919a93212
shuyu626 <164139722+shuyu626@users.noreply.github.com>
2024-10-13 15:28:07 +0000

    Deploying to gh-pages from @ shuyu626/final_front@84c3230fbd3ae86e54641a2336669bc9e00f0208 🚀

---
+++ assets/appButton-D5guv0Qd.js
@@ -0,0 +1,1 @@
+import{o as e,e as a,m as n}from"./index-COBXCSfB.js";const p={__name:"appButton",props:{text:{type:String,default:"新增"}},setup(t){return(o,r)=>(e(),a(n,{type:"submit",text:t.text,variant:"text",class:"rounded-xl b-1",style:{"font-weight":"630"},density:"comfortable",ripple:!1},null,8,["text"]))}};export{p as _};
