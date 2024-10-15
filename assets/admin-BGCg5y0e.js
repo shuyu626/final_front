f36882c4406247c601627c4497e95e42cdac8b5e
shuyu626 <164139722+shuyu626@users.noreply.github.com>
2024-10-15 13:54:25 +0000

    Deploying to gh-pages from @ shuyu626/final_front@1023564225574ad4b2ef437aa01466c9d227a4e8 🚀

--- assets/admin-BGCg5y0e.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{V as r}from"./VMain-DGWbmyN7.js";import{r as a,o as n,c,a as s,b as e,w as m,F as _}from"./index-COBXCSfB.js";import"./ssrBoot-CNgd7gji.js";const i={};function l(f,p){const o=a("router-view");return n(),c(_,null,[s("d "),e(r,null,{default:m(()=>[e(o)]),_:1})],64)}const V=t(i,[["render",l]]);export{V as default};
